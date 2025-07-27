f21e4ae7d1a17a11909a3be19d83580c26ce4570
Aditya <adityabasak7031@gmail.com>
2025-07-27 23:04:06 +0530

    add signup API

---
+++ app/api/(auth)/signup/route.ts
@@ -0,0 +1,51 @@
+import { client } from "@/db";
+import bcrypt from "bcryptjs";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+
+    try {
+        const body = await req.json();
+        const { name, email, password } = body;
+
+        if (!name || !email || !password) {
+            return NextResponse.json({
+                message: "All fields are required"
+            }, { status: 400 });
+        }
+
+        const existingUser = await client.user.findUnique({
+            where: { email }
+        });
+
+        if (existingUser) {
+            return NextResponse.json({
+                message: "Email already exists"
+            }, { status: 400 });
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = await client.user.create({
+            data: {
+                name,
+                email,
+                password: hashedPassword
+            }
+        });
+
+        const { password: _, ...userWithoutPassword } = newUser;
+
+        return NextResponse.json({
+            message: "user created successfully",
+            user: userWithoutPassword
+        }, { status: 201 });
+
+    } catch (err) {
+        return NextResponse.json({
+            message: "failed to signup",
+            details: err instanceof Error ? err.message : err
+        }, { status: 400 });
+    }
+}
