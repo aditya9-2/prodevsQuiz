af50334f14ea239240ed978302a4eeadf9bb213b
Aditya <adityabasak7031@gmail.com>
2025-07-28 13:46:34 +0530

    add role in body

--- app/api/(auth)/signup/route.ts
+++ app/api/(auth)/signup/route.ts
@@ -6,7 +6,7 @@ export async function POST(req: NextRequest) {
 
     try {
         const body = await req.json();
-        const { name, email, password } = body;
+        const { name, email, password, role } = body;
 
         if (!name || !email || !password) {
             return NextResponse.json({
@@ -31,7 +31,8 @@ export async function POST(req: NextRequest) {
             data: {
                 name,
                 email,
-                password: hashedPassword
+                password: hashedPassword,
+                role: role || "USER"
             }
         });
 
